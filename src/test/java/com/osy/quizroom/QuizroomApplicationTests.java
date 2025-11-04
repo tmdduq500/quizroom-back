99ce82e9ec4e8f3cd1f934b8150eed422be93847
osy9907 <osy9907@naver.com>
2025-11-04 09:50:19 +0900

    quizroom 초기화

---
+++ src/test/java/com/osy/quizroom/QuizroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osy.quizroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
